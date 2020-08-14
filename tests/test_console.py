5de1047bef39028920b4ccc522e38761d9573042
nourouhichi <1417@holbertonschool.com>
2020-08-14 11:46:23 +0100

    add test

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+import console
+import unittest
+HBNBCommand = console.HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """" testing console docs"""
+    def test_docstring(self):
+        """docstring validation test"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
+
