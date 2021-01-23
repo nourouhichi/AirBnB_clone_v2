207bc18db3e82caefbb68d9fb5a5e1c1cc8dacd6
nourouhichi <1417@holbertonschool.com>
2021-01-23 18:34:37 +0100

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def Hello_HBNB():
     """returning a message"""
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,15 +8,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def Hello_HBNB():
     """returning a message"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returning a mesg"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
