c61f7218bfaf02c2c44939c60433cfc637038a16
nourouhichi <60342914+nourouhichi@users.noreply.github.com>
2021-01-27 18:18:40 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,17 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     id = Column(String(60), nullable=False, unique=True, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -19,12 +23,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+            for k, v in kwargs.items():
+                setattr(self, k, v)
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -43,14 +50,14 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        if "_sa_instance_state" in dictionary.keys():
-            dictionary.pop(key["_sa_instance_state"])
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if dictionary[sa_instance_state]:
+            del dictionary[sa_instance_state]
         return dictionary
 
     def delete(self):
-        """delete the current instance from the storage """
+        """deleting an instance"""
         models.storage.delete(self)
