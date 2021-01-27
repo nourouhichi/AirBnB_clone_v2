3b5edc1611729011ca4a9b2fdc5a767a288cdf32
nourouhichi <1417@holbertonschool.com>
2021-01-27 18:44:56 +0100

    new push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,74 +40,7 @@ class FileStorage:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
-                temp[key] = val.#!/usr/bin/python3
-"""This is the database storage class for AirBnB"""
-
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-class DBStorage:
-    """class that uses mysql for datastorage"""
-    __engine = None
-    __session = None
-
-    def __init__(self):
-        self.__engine = create_engine(
-           'mysql+mysqldb://{}:{}@{}/{}'.format(
-                os.getenv("HBNB_MYSQL_USER"),
-                os.getenv("HBNB_MYSQL_PWD"),
-                os.getenv("HBNB_MYSQL_HOST"),
-                os.getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
-        if os.getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        instances={}
-        if cls:
-            objs = self.__session.query(cls).all()
-            for obj in objs:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                instances[key] = obj
-        else:
-            classes = ["State", "City"]
-            for classy in classes:
-                objs = self.__session.query(eval(classy))
-                for obj in objs:
-                    key = "{}.{}".format(type(obj).__name__, obj.id)
-                    instances[key] = obj
-        return instances
-
-    def new(self, obj):
-        """ adding to the new session"""
-        self.__session.add(obj)
-
-    def save(self):
-        """commiting all changes """
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """ delete from the current database session"""
-        if obj is not None:
-            self.__session.delete(obj)
-
-    def reload(self):
-        """ reload all the objs"""
-        Base.metadata.create_all(self.__engine)
-        Session = scoped_session(sessionmaker(
-            bind=self.__engine, expire_on_commit=False))
-        self.__session = Session()
-
-    def close(self):
-        """Call close() method on the class Session"""
-        self.__session.close()to_dict()
+                temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
