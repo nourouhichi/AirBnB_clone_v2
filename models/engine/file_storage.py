7443c9c2f0abb4eedfe78008b2a198837bbe6283
nourouhichi <60342914+nourouhichi@users.noreply.github.com>
2021-01-27 18:19:46 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,14 +10,25 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         if cls is None:
-            return FileStorage.__objects
-        else:
-            dic = {}
-            for key, value in self.__objects.items():
-                if value.__class__ == cls:
-                    dic[key] = value
-            return dic
+            return self.__objects
+        cls_list={}
+        for k, v in self.__objects.items():
+            if v.__class__ is cls:
+                cls_list[k] = v
+        return cls_list
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -29,7 +40,74 @@ class FileStorage:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
-                temp[key] = val.to_dict()
+                temp[key] = val.#!/usr/bin/python3
+"""This is the database storage class for AirBnB"""
+
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+class DBStorage:
+    """class that uses mysql for datastorage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine(
+           'mysql+mysqldb://{}:{}@{}/{}'.format(
+                os.getenv("HBNB_MYSQL_USER"),
+                os.getenv("HBNB_MYSQL_PWD"),
+                os.getenv("HBNB_MYSQL_HOST"),
+                os.getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        instances={}
+        if cls:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                instances[key] = obj
+        else:
+            classes = ["State", "City"]
+            for classy in classes:
+                objs = self.__session.query(eval(classy))
+                for obj in objs:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    instances[key] = obj
+        return instances
+
+    def new(self, obj):
+        """ adding to the new session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commiting all changes """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ reload all the objs"""
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(
+            bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
+
+    def close(self):
+        """Call close() method on the class Session"""
+        self.__session.close()to_dict()
             json.dump(temp, f)
 
     def reload(self):
@@ -52,13 +130,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
+    
     def delete(self, obj=None):
-        """ deletes obj from __objects if exists"""
+        """destroying an object"""
         if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            key = "{}.{}".format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
